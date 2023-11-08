f7b11903cb77fa5ef80ed7df599087b5bd0df76f
TURAEVA SHOKHSANAM <117541374+Sanam02@users.noreply.github.com>
2023-11-08 11:55:13 -0600

    Create main.tf
    main.tf modified

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.13.1"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  availability_zones = ["${var.aws_region}a", "${var.aws_region}b"]
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.public_subnets_cidr)
+  cidr_block              = element(var.public_subnets_cidr, count.index)
+  availability_zone       = element(local.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-${element(local.availability_zones, count.index)}-public-subnet"
+    Environment = "${var.environment}"
+  }
+}
